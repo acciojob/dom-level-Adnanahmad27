64c5c131a9a8aecc2b772409a2d93a6ab082e63d
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-08 18:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+
+let n = getDOMLevel();
+alert(`The level of the element is: ${n}`)
+function getDOMLevel(e) {
+	const tragetElement = document.getElementById("level");
+	let domLevel = 0;
+	hasParentElement(tragetElement);
+	function hasParentElement(element) {
+		if(element.parentNode){
+			domLevel++;
+			hasParentElement(element.parentNode);
+		}
+	}
+	return domLevel;
+}
